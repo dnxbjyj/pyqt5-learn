559f9bebf246fad91a567b1de21fc1d762be4a25
dnxbjyj <dnxbjyj@126.com>
2019-04-30 00:42:07 +0800

    add basic signal and slots

---
+++ basic-demo/EventAndSignal/EventObject.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+# status: TEST PASSED
+
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QApplication, QGridLayout, QLabel)
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+
+        x = 0
+        y = 0
+        self.text = "x: {0}, y: {1}".format(x, y)
+
+        self.label = QLabel(self.text, self)
+        grid.addWidget(self.label, 0, 0, Qt.AlignTop)
+
+        # [KEY POINT]
+        self.setMouseTracking(True)
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 200)
+        self.setWindowTitle('Event Object')
+        self.show()
+
+    # [KEY POINT]
+    def mouseMoveEvent(self, e):
+        x = e.x()
+        y = e.y()
+        text = "x: {0}, y: {1}".format(x, y)
+        self.label.setText(text)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+    
+
